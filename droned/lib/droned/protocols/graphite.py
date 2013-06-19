ae0fe7e2d8195062c4951cf63cd414cdea2806a9
Jacob Richard <jacob.richard@gmail.com>
2013-06-18 19:22:42 -0500

    Breaking out graphite protocol classes as a simple solution to Receiver issues

--- droned/lib/droned/protocols/graphite.py
+++ droned/lib/droned/protocols/graphite.py
@@ -13,33 +13,33 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 ###############################################################################
-
+ 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 from twisted.internet.protocol import Protocol
 from twisted.python.failure import Failure
-from twisted.protocols.basic import LineReceiver
-
-class GraphiteProtocol(LineReceiver):
-    """base protocol for sending metrics to a Graphite Receiver"""
+from twisted.protocols.basic import LineReceiver, Int32StringReceiver
+ 
+ 
+class PickleEmitter(Int32StringReceiver):
+    """Send Graphite Data in Pickle Receiver Format"""
+ 
     metric = property(lambda s: s.get_metric())
+ 
     def __init__(self, *args, **kwargs):
         self.metricData = args[0]
         self.deferred = args[-1] #deferred must always be the last argument
         self.METRIC_SENT = False
-
-    def get_metric(self):
-        raise NotImplemented('you must implement this')
-
+ 
     def connectionMade(self):
         data = self.metric
         if data: #if we don't have data don't write
-            self.sendLine(data)        
+            self.sendString(data)
             self.METRIC_SENT = True
         self.transport.loseConnection()
-
+ 
     def connectionLost(self, reason):
         if self.deferred.called:
             return
@@ -47,16 +47,36 @@ class GraphiteProtocol(LineReceiver):
            self.deferred.errback(reason)
         else:
            self.deferred.callback(None)
-
-
-class PickleEmitter(GraphiteProtocol):
-    """Send Graphite Data in Pickle Receiver Format"""
+ 
     def get_metric(self):
         return pickle.dumps(self.metricData)
-
-
-class LineEmitter(GraphiteProtocol):
+ 
+ 
+class LineEmitter(LineReceiver):
     """Send Graphite Data in Line Receiver Format"""
+ 
+    metric = property(lambda s: s.get_metric())
+ 
+    def __init__(self, *args, **kwargs):
+        self.metricData = args[0]
+        self.deferred = args[-1] #deferred must always be the last argument
+        self.METRIC_SENT = False
+ 
+    def connectionMade(self):
+        data = self.metric
+        if data: #if we don't have data don't write
+            self.sendLine(data)
+            self.METRIC_SENT = True
+        self.transport.loseConnection()
+ 
+    def connectionLost(self, reason):
+        if self.deferred.called:
+            return
+        if not self.METRIC_SENT:
+           self.deferred.errback(reason)
+        else:
+           self.deferred.callback(None)
+ 
     def get_metric(self):
         metrics = []
         for (metric, (value, stamp)) in self.metricData:
@@ -65,5 +85,5 @@ class LineEmitter(GraphiteProtocol):
         if metrics and not metrics.endswith('\n'):
             metrics += '\n'
         return metrics
-
+ 
 __all__ = ['LineEmitter', 'PickleEmitter']
