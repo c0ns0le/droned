5b0aafd16b197280ff502f307aa92727060354b6
Jacob Richard <jacob.richard@gmail.com>
2014-02-25 14:49:06 -0600

    Revising Logger for Proper Log Rotation Based on Oldest Logs

--- droned/lib/droned/logging.py
+++ droned/lib/droned/logging.py
@@ -13,22 +13,22 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 ###############################################################################
-
+ 
 """This module implements DroneD's logging facilities."""
 import sys, os, time, glob
 from twisted.python.failure import Failure
 from twisted.python.log import FileLogObserver, StdioOnnaStick
 from twisted.python.logfile import DailyLogFile
-
+ 
 class DailyPurgingLogFile(DailyLogFile):
     """A log file that is rotated daily (at or after midnight localtime).
-
+ 
        A maxRotatedFiles of None disables purging.
     """
     def __init__(self, *args, **kwargs):
         """
         Create a log file rotating on length.
-
+ 
         @param name: file name.
         @type name: C{str}
         @param directory: path of the log file.
@@ -42,28 +42,32 @@ class DailyPurgingLogFile(DailyLogFile):
         self.maxRotatedFiles = kwargs.pop('maxRotatedFiles', None)
         DailyLogFile.__init__(self, *args, **kwargs)
         self._logger = logWithContext(type='console')
-
+ 
     def notification(self, message):
         """
         only used when logs are deleted
         """
         self._logger(message)
-
+ 
     def listLogs(self):
         """
-        Return sorted list of integers - the old logs' identifiers.
+        Return sorted list of logfile names.
         """
-        result = glob.glob("%s.*" % self.path)
-        result.sort()
-        return result
-
+        logs = glob.glob("%s.*" % self.path)
+ 
+        def sort_algorithm(name):
+            timeobj = time.strptime(name, self.path + '.%Y_%m_%d')
+            return time.mktime(timeobj)
+ 
+        return sorted(logs, key=sort_algoritm)
+ 
     def rotate(self):
         """
         Rotate the file and create a new one.
-
+ 
         If it's not possible to open new logfile, this will fail silently,
         and continue logging to old logfile.
-
+ 
         Old log files will be automatically purged.
         """
         #daily rotation first
@@ -79,26 +83,26 @@ class DailyPurgingLogFile(DailyLogFile):
             if l.endswith('log'): continue
             self.notification('deleting %s' % l)
             os.remove(l)
-
-
+ 
+ 
 class MyLogObserver(FileLogObserver):
     timeFormat = "[%Y-%m-%d %H:%M:%S]"
-
-
+ 
+ 
 class StdioKabob(StdioOnnaStick):
     def write(self, data):
         data = (self.buf + data).split('\n')
         self.buf = data[-1]
         for message in data[:-1]:
             log(message)
-
+ 
     def writelines(self, lines):
         for line in lines:
             self.write(line)
-
+ 
 #Initialization API
 logs = {}
-
+ 
 def logToStdout(timestamp=False):
     """Call this to cause all log messages to go to stdout"""
     logs['console'] = MyLogObserver(sys.stdout)
@@ -106,8 +110,8 @@ def logToStdout(timestamp=False):
         logs['console'].timeFormat = "" #get rid of that
     sys.stdout = StdioKabob(0)
     sys.stderr = StdioKabob(1)
-
-
+ 
+ 
 def logToDir(directory='logs', LOG_TYPE=('console',), OBSERVER=MyLogObserver):
     """Call this to write logs to the specified directory,
        optionally override the FileLogObserver.
@@ -122,8 +126,8 @@ def logToDir(directory='logs', LOG_TYPE=('console',), OBSERVER=MyLogObserver):
         path = os.path.join(directory, name + '.log')
         logfile = DailyPurgingLogFile.fromFullPath(path, *args, **kwargs)
         logs[name] = OBSERVER(logfile)
-
-
+ 
+ 
 #Logging API
 def log(message="", **context):
     """Log a message, with some optional context parameters"""
@@ -135,8 +139,8 @@ def log(message="", **context):
         destination.emit(event)
     else:
         print "<<<UNHANDLED LOG EVENT>>> %s" % str(event)
-
-
+ 
+ 
 def logWithContext(**context):
     """Create a log() function that assumes the given context parameters by default"""
     destination = get_destination(context)
@@ -146,8 +150,8 @@ def logWithContext(**context):
         myContext.update(kwargs)
         log(message, **myContext)
     return _log
-
-
+ 
+ 
 #Internal functions
 def create_log_event(message, context):
     """Internal function used to convert droned log events to Twisted log events"""
@@ -158,34 +162,34 @@ def create_log_event(message, context):
     event['message'] = message
     event['time'] = time.time()
     event['system'] = context.get('system') or context.get('type') or 'console'
-
+ 
     if event.get('error'):
         event['isError'] = True
-
+ 
     if event.get('warning'):
         event['message'] = '[WARNING] %s' % event['message']
-
+ 
     if event.get('excessive') and config and not config.EXCESSIVE_LOGGING:
         event['discard'] = True
-
+ 
     event['message'] = (event['message'],) #stupid hack for twisted...
     return event
-
-
+ 
+ 
 def get_destination(event):
     """Internal function used to route a log event to a particular LogObserver (log file)"""
     if 'type' in event and event['type'] in logs:
         return logs[ event['type'] ]
-
+ 
     elif 'route' in event and event['route'] in logs:
         return logs[ event['route'] ]
-
+ 
     return logs.get('console')
-
-
+ 
+ 
 #Conveniences
 debug = logWithContext(type='debug')
-
+ 
 def err(message="", **context):
     failure = Failure()
     if message:
